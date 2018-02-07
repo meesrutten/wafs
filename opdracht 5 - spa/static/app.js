189b3b069d5c1222a3eef5981473475fa8ce36e1
Cas Burggraaf <casburggraaf@gmail.com>
2018-02-07 11:56:28 +0100

    Basis app structure

---
+++ opdracht 5 - spa/static/app.js
@@ -0,0 +1,28 @@
+// Create local scope
+(function() {
+  //Init Aplication
+  var app = {
+    init: function() {
+      console.log("Init app sucsesfol");
+    },
+    rootElement: document.body
+  };
+
+  // Handle routes and states
+  var routers = {
+    init: function() {
+
+    }
+  }
+
+  // Render / toggle sections
+  var sections = {
+    toggle: function() {
+
+    }
+  }
+
+  // Start the Aplication
+  app.init();
+
+})();
