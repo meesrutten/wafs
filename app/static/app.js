55389568b6f3bc83ee9560bc772281cad7311dfe
Cas Burggraaf <casburggraaf@gmail.com>
2018-02-12 11:10:55 +0100

    added simple api get call(opdracht 1)

--- app/static/app.js
+++ app/static/app.js
@@ -4,6 +4,7 @@
   var app = {
     init: function() {
       routers.init();
+      xhr.request();
     },
     rootElement: document.body
   };
@@ -35,6 +36,34 @@
           this.sections[i].classList.add("active");
         }
       }
+    }
+  }
+
+
+  var xhr = {
+    apiBasisUrl: "http://api.themoviedb.org/3/movie/76341",
+    apiKey: "d9a167a57e748b4a804b41f0186b2339",
+    data: {},
+    request: function() {
+      var request = new XMLHttpRequest();
+      request.open('GET', `${this.apiBasisUrl}?api_key=${this.apiKey}`, true);
+
+      request.onload = function() {
+        if (request.status >= 200 && request.status < 400) {
+         // Success!
+          this.data = JSON.parse(request.responseText);
+          console.log(this.data);
+        } else {
+         // We reached our target server, but it returned an error
+
+        }
+      };
+
+      request.onerror = function() {
+       // There was a connection error of some sort
+      };
+
+      request.send();
     }
   }
 
