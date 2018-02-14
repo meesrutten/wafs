26fec868bd77ba7976d20ea920f0aa514cb77dc2
Cas Burggraaf <casburggraaf@gmail.com>
2018-02-14 11:26:16 +0100

    Added Xo javascript linter and fixed 'some' bugs

--- app/static/app.js
+++ app/static/app.js
@@ -1,26 +1,26 @@
 // Create local scope
 (function() {
-  //Init Aplication
-  var app = {
-    init: function() {
+  // Init Aplication
+  const app = {
+    init() {
       routers.init();
     },
     rootElement: document.body
   };
 
   // Handle routes and states
-  var routers = {
-    init: function() {
+  const routers = {
+    init() {
       // Make routie config and requesting a xhr if needed
       routie({
-        'start': function() {
+        start: function() {
           sections.toggle(arguments.callee.name);
         },
-        'popular': function() {
+        popular: function() {
           sections.toggle(arguments.callee.name);
           xhr.request("popular");
         },
-        'movie/:movieId': function (movieId) {
+        "movie/:movieId": function (movieId) {
           sections.toggle("movieDetail");
           xhr.request(movieId);
         }
@@ -29,46 +29,46 @@
   };
 
   // Render / toggle sections
-  var sections = {
+  const sections = {
     sectionsElements: app.rootElement.querySelectorAll("body>section"),
-    toggle: function(route) {
+    toggle(route) {
       this.sectionsElements.forEach(function (el) {
         el.classList.remove("active");
-        if (el.id == route) {
+        if (el.id === route) {
           el.classList.add("active");
         }
       });
     }
-  }
+  };
 
-  var xhr = {
+  const xhr = {
     apiBasisUrl: "https://api.themoviedb.org/3/movie/",
     apiKey: "d9a167a57e748b4a804b41f0186b2339",
     data: {},
-    request: function(apiSearchParm) {
+    request(apiSearchParm) {
       var request = new XMLHttpRequest();
       var _this = this;
       // Making the url and creating a GET request
-      var url = `${this.apiBasisUrl}${apiSearchParm}?api_key=${this.apiKey}`
+      var url = `${this.apiBasisUrl}${apiSearchParm}?api_key=${this.apiKey}`;
 
       request.open('GET', url, true);
 
       request.onload = function() {
         if (request.status >= 200 && request.status < 400) {
           // Success!
-          //TODO Change xhr to THIS
+          // TODO Change xhr to THIS
           // Putting results into the data propoty and render page
           xhr.data = JSON.parse(request.responseText);
           xhr.render(apiSearchParm);
         } else {
-         // We reached our target server, but it returned an error
+          // We reached our target server, but it returned an error
         }
       };
       request.send();
     },
-    render: function (apiSearchParm) {
-      //Checking if it a popular Search or Movie detail page
-      if(apiSearchParm == "popular"){
+    render(apiSearchParm) {
+      // Checking if it a popular Search or Movie detail page
+      if (apiSearchParm === "popular") {
         var directives = {
           title: {
             href: function(params) {
@@ -84,7 +84,7 @@
         var directives = {
           title: {
             href: function (params) {
-              return `https://imdb.com/title/${this.imdb_id}`
+              return `https://imdb.com/title/${this.imdb_id}`;
             },
             text: function (params) {
               return `${this.title}(${this.release_date.substring(0, 4)})`;
@@ -102,9 +102,8 @@
         Transparency.render(target, this.data, directives);
       }
     }
-  }
+  };
 
   // Start the Aplication
   app.init();
-
 })();
